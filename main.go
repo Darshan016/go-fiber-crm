91986e9e7daa250d50a29361aaac34841c1d229d
darshan panchal <panchaldarshan121@gmail.com>
2024-06-29 15:57:16 +0530

    added routes

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Darshan016/go-fiber-crm/database"
+	"github.com/Darshan016/go-fiber-crm/lead"
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead:id", lead.DeleteLead)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to database!")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database migrated!")
+}
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(8000)
+	defer database.DBConn.Close()
+}
